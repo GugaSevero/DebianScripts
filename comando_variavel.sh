86c6d4e6d0178c72ee30923e12d2bf70a958f794
root <gustavo.msevero@gmail.com>
2016-06-21 21:35:12 -0400

    Novos arquivos de script

---
+++ comando_variavel.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+system_info='df -h' # Também poderia ser system_info=$(df -h)
+echo "$system_info"
---
+++ exemplo1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Seu nome de usuário é:"
+whoami
+echo "Info da hora atual e tempo que o computador está ligado:"
+uptime
+echo " O script está executando do diretório:"
+pwd
